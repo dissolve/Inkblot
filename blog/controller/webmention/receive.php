4ac36655c11db883300866d7ebf7103e54c65ffb
Ben Roberts <ben@thatmustbe.me>
2014-10-13 11:51:37 -0400

    starting vouch receiving

--- blog/controller/webmention/receive.php
+++ blog/controller/webmention/receive.php
@@ -1,23 +1,74 @@
 <?php  
 class ControllerWebmentionReceive extends Controller {
     public function index() {
-        if(isset($this->request->post['source']) && isset($this->request->post['target'])){
+        $source = $this->request->post['source'];
+        $target = $this->request->post['target'];
+        $vouch = $this->request->post['vouch'];
+
+        // are URLs Valid?
+        if(!$this->is_valid_url($source) || !$this->is_valid_url($target)){
+            header('HTTP/1.1 400 Bad Request');
+            exit();
+        } elseif (!$this->is_approved_source($source)){
+
+            if(!USE_VOUCH){
+                header('HTTP/1.1 400 Bad Request');
+                exit();
+            } else {
+                if(!isset($this->request->post['vouch'])){
+                    header('HTTP/1.1 449 Retry With vouch');
+                    exit();
+
+                } elseif(!$this->is_valid_url($vouch)){
+                    header('HTTP/1.1 400 Bad Request');
+                    exit();
+                } elseif(!$this->is_approved_source($vouch)){
+                    header('HTTP/1.1 400 Bad Request');
+                    exit();
+
+                } else {
+
+                //todo vouch workflow
+                }
+
+            }
+            
+        } else {
+
             $this->response->addHeader('HTTP/1.1 202 Accepted');
 
             $this->load->model('webmention/queue');
-            $queue_id = $this->model_webmention_queue->addEntry($this->request->post['source'], $this->request->post['target']);
+            $queue_id = $this->model_webmention_queue->addEntry($source, $target);
+
             if(isset($this->request->post['callback'])){
                 $this->model_webmention_queue->setCallback($queue_id, $this->request->post['callback']);
             }
+
             $link = $this->url->link('webmention/queue', 'id='.$queue_id, '');
 
             $this->response->addHeader('Link: <'.$link.'>; rel="status"');
 
             $this->response->setOutput($link);
-        } else {
-            header('HTTP/1.1 400 Bad Request');
-            exit();
         }
+    }
+
+    private function is_approved_source($url){
+        //todo
+        return true;
+    }
+
+    //very basic function to determine if URL is valid, this is certainly a great place for improvement
+    private function is_valid_url($url){
+        if(!isset($url)) {
+            return false;
+        }
+        if(empty($url)) {
+            return false;
+        } 
+        if(strpos($url, '.') == 0) {
+            return false;
+        }
+        return true;
     }
 
 }
