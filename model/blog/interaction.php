72b504179572e7c739697902286f3febcc35a915
Ben Roberts <ben@thatmustbe.me>
2015-04-03 09:40:19 -0400

    allow for receiving tags

--- model/blog/interaction.php
+++ model/blog/interaction.php
@@ -23,43 +23,57 @@ class ModelBlogInteraction extends Model {
                 $post = $this->model_blog_post->getPostByDayCount($data['year'],$data['month'], $data['day'], $data['daycount']);
             }
 
-        $interaction_type = 'mention';
+                $interaction_type = 'mention';
 
             switch($comment_data['type']) {
             case 'like':
-        $interaction_type = 'like';
+                $interaction_type = 'like';
                 break;
             case 'reply':
-        $interaction_type = 'reply';
+                $interaction_type = 'reply';
                 break;
             case 'repost':
-        $interaction_type = 'repost';
+                $interaction_type = 'repost';
                 break;
             case 'tag':
-        $interaction_type = 'tag';
+                $interaction_type = 'tag';
                 break;
             case 'rsvp':
-        $interaction_type = 'rsvp';
+                $interaction_type = 'rsvp';
                 break;
             //case 'mention':
             //default:
         }
 
+        //TODO: move this to some config setting
+        $autoapprove = true;
 
         $this->db->query("INSERT INTO ". DATABASE.".interactions SET source_url = '".$comment_data['url']."'".
-        ((isset($comment_data['author']) && isset($comment_data['author']['name']) && !empty($comment_data['author']['name']))? ", author_name='".$comment_data['author']['name']."'" : "") .
-        ((isset($comment_data['author']) && isset($comment_data['author']['url']) && !empty($comment_data['author']['url']))? ", author_url='".$comment_data['author']['url']."'" : "") .
-        ((isset($comment_data['author']) && isset($comment_data['author']['photo']) && !empty($comment_data['author']['photo']))? ", author_image='".$comment_data['author']['photo']."'" : "") .
-        ((isset($comment_data['tag-of']) && !empty($comment_data['tag-of']))? ", tag_of='".$comment_data['tag-of']."'" : "") .
+                ((isset($comment_data['author']) && isset($comment_data['author']['name']) && !empty($comment_data['author']['name']))? ", author_name='".$comment_data['author']['name']."'" : "") .
+                ((isset($comment_data['author']) && isset($comment_data['author']['url']) && !empty($comment_data['author']['url']))? ", author_url='".$comment_data['author']['url']."'" : "") .
+                ((isset($comment_data['author']) && isset($comment_data['author']['photo']) && !empty($comment_data['author']['photo']))? ", author_image='".$comment_data['author']['photo']."'" : "") .
+                ((isset($comment_data['tag-of']) && !empty($comment_data['tag-of']))? ", tag_of='".$comment_data['tag-of']."'" : "") .
                 ((isset($comment_data['text'])  && !empty($comment_data['text']))? ", body='".$this->db->escape($comment_data['text'])."'" : "") .
                 ((isset($comment_data['name'])  && !empty($comment_data['name']))? ", source_name='".$this->db->escape($comment_data['name'])."'" : "") .
                 ((isset($comment_data['published'])  && !empty($comment_data['published']))? ", `timestamp`='".$this->db->escape($comment_data['published'])."'" : ", `timestamp`=NOW()") .
-        ", webmention_id='".$webmention_id."'".
-        ", interaction_type='".$interaction_type."'".
-                ", post_id = ".(int)$post['post_id'].
-        ", parse_timestamp = NOW()".
-        ", approved=1".
+                ", webmention_id='".$webmention_id."'".
+                ", interaction_type='".$interaction_type."'".
+                        ", post_id = ".(int)$post['post_id'].
+                ", parse_timestamp = NOW()".
+                ($autoapprove ? ", approved=1" : '' ) .
         "");
+
+        if($autoapprove && $type=='tag'){
+            foreach($comment_data['tags'] as $tag){
+                if(isset($tag['category'])){
+                    $this->model_blog_post->addToCategory($post['post_id'], $tag['category']);
+                } elseif(isset($tag['url'])){
+                    $this->model_blog_post->addToCategory($post['post_id'], $tag['url']);
+                }
+            }
+
+        }
+
         $interaction_id = $this->db->getLastId();
         $this->db->query("UPDATE ". DATABASE.".webmentions SET webmention_status_code = '200', webmention_status = 'OK' WHERE webmention_id = ". (int)$webmention_id);
         $this->cache->delete('interactions');
