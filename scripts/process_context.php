57f6f810596957916f71ca7612c0136c39724d7e
BenRoberts <ben@cs.uri.edu>
2015-11-05 13:25:53 -0500

    remove not found action on scripts from CLI

--- scripts/process_context.php
+++ scripts/process_context.php
@@ -1,4 +1,4 @@
 <?php
 require_once('script_setup.php'); //this is to set up interactions to the MVC
 $action = new Action('webmention/queue/processcontexts');
-$controller->dispatch($action, new Action('error/not_found'));
+$controller->dispatch($action);
