13923d918f01f3837bc3c9ab897d9bdcbd56e36a
BenRoberts <ben@cs.uri.edu>
2015-12-15 23:00:39 -0500

    storing reacji correctlygit add model/blog/interaction.php

--- model/blog/interaction.php
+++ model/blog/interaction.php
@@ -1,4 +1,5 @@
 <?php
+require_once DIR_BASE . 'libraries/single-emoji-recognizer/src/emoji.php';
 class ModelBlogInteraction extends Model {
 
     public function addWebmention($data, $webmention_id, $comment_data, $post_id = null)
@@ -52,9 +53,16 @@ class ModelBlogInteraction extends Model {
             switch ($comment_data['type']) {
                 case 'like':
                     $interaction_type = 'like';
+                    $interaction_type = 'reacji';
+                    $comment_data['text'] = html_entity_decode('&#10084;'); //a heart emoji
                 break;
                 case 'reply':
+                    $body_text = trim($comment_data['text']);
                     $interaction_type = 'reply';
+                    if(EmojiRecognizer::isSingleEmoji($body_text)) {
+                        $interaction_type = 'reacji';
+                        $comment_data['text'] = $body_text;
+                    }
                 break;
                 case 'repost':
                     $interaction_type = 'repost';
